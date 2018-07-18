081fb3a97aaf82343a340345062b5864540d5dc8
edbeeching <edbeeching@aol.com>
2018-07-18 15:47:59 +0200

    finished saliency maps

--- simple_saliency.py
+++ simple_saliency.py
@@ -21,76 +21,80 @@ from skimage.io import imread
 from torchvision import transforms
 from torchvision.utils import make_grid
 
-forward_inputs  = []
-forward_outputs  = []
-backward_inputs  = []
-backward_outputs  = []
+from cv2 import resize
 
 
-def forward_hook(self, input, output):
-    print(input[0].size())
-    print(output[0].size())
-    
-    forward_inputs.append(input[0].clone())
-    forward_outputs.append(output[0].clone())
-    
-    
-def backward_hook(self, grad_input, grad_output):
-    print(grad_input[0].size())
-    print(grad_output[0].size())
-    backward_inputs.append(grad_input[0].clone())
-    backward_outputs.append(grad_output[0].clone())
+#print('adding hook')
+#
+#print(model.features)
+#
+## load an image
+#
+#img = imread('both.png')
+#plt.imshow(img)
+#img = preproc(img)
 
-preproc = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+#assert 0 
+#
+#import json
+#class_idx = json.load(open("imagenet_class_index.json"))
+#idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
+#
+#
+#grid = make_grid(forward_outputs[0].data.view(512,1,14,14), nrow=32).numpy().transpose(1,2,0)
+#grid_back = make_grid(backward_outputs[0].data.view(512,1,14,14), nrow=32).numpy().transpose(1,2,0)
+#
+#plt.imshow(grid[:,:,0])
+#plt.imshow(grid_back[:,:,0])
+
+#plt.imshow(F.relu(Variable(res)).data.numpy())
+#
+#upscaled = resize(F.relu(Variable(res)).data.numpy(), (224, 224))
+#
+#plt.imshow(imread('both.png'))
+#
+#plt.imshow(upscaled, alpha=0.9, cmap='gray')
+
+def get_salency(model, image, layer=-2, index=None):
+    preproc = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    img = preproc(image)
+    forward_outputs  = []
+    backward_outputs  = []    
+    
+    def forward_hook(self, input, output):
+        forward_outputs.append(output[0].clone())
+        
+        
+    def backward_hook(self, grad_input, grad_output):
+        backward_outputs.append(grad_output[0].clone())    
+        
+    model.features[layer].register_forward_hook(forward_hook)
+    model.features[layer].register_backward_hook(backward_hook) 
+      
+    preds = model(Variable(img.unsqueeze(0)))
+    
+    if index == None:    
+        index = np.argmax(preds.data.numpy())
+    gradient_vector = torch.zeros_like(preds)
+    gradient_vector[0, index] = 1.0
+    
+    loss = (preds*gradient_vector).sum()
+    loss.backward()    
+
+    forwards = forward_outputs[0].data
+    backwards = backward_outputs[0][0].data
+ 
+    res = (forwards * backwards).mean(dim=0) 
+
+    upscaled = resize(F.relu(Variable(res)).data.numpy(), (224, 224))
+    
+    return upscaled
 
 model = torchvision.models.vgg19(pretrained=True).eval()
-print('adding hook')
-model.features[-2].register_forward_hook(forward_hook)
-model.features[-2].register_backward_hook(backward_hook)
+image = resize(imread('cat.jpeg'), (224,224))
 
-print(model.features)
+saliency = get_salency(model, image)
 
-# load an image
-
-img = imread('horse.jpg')
-plt.imshow(img)
-img = preproc(img)
-
-preds = model(Variable(img.unsqueeze(0)))
-
-index = np.argmax(preds.data.numpy())
-gradient_vector = torch.zeros_like(preds)
-gradient_vector[0, index] = 1.0
-
-loss = (preds*gradient_vector).sum()
-loss.backward()
-
-
-
-assert 0 
-
-import json
-class_idx = json.load("imagenet_class_index.json")
-idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
-
-
-grid = make_grid(forward_outputs[0].data.view(512,1,14,14), nrow=32).numpy().transpose(1,2,0)
-grid_back = make_grid(backward_outputs[0].data.view(512,1,14,14), nrow=32).numpy().transpose(1,2,0)
-
-plt.imshow(grid[:,:,0])
-plt.imshow(grid_back[:,:,0])
-
-forwards = forward_outputs[0].data
-backwards = backward_outputs[0][0].data
-
-res = (forwards * backwards).mean(dim=0)
-
-plt.imshow(F.relu(Variable(res)).data.numpy())
-
-
-
-
-
-
-
-
+plt.imshow(image)
+plt.imshow(saliency, alpha=0.9, cmap='gray')
